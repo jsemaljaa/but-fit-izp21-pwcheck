9a79a0da895f50bad7be2499dea17d9168da2e02
jsemaljaa <jsemaljaa@gmail.com>
2021-09-23 12:44:21 +0200

    added test txt

---
+++ pwcheck.c
@@ -0,0 +1,10 @@
+                                        /*
+                                        -----------------------------------
+                                        BUT FIT 2021/2022, winter semester 
+                                        Introduction to Programming Systems
+                                        Project #1 (password check)
+                                        
+                                        Author: Alina Vinogradova
+                                        E-mail: xvinog00@stud.fit.vutbr.cz
+                                        -----------------------------------
+                                                                         */
