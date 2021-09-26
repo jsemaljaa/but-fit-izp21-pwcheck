bd90a3c0f592aeaba9267a6799fd2efdf48bc1d0
jsemaljaa <jsemaljaa@gmail.com>
2021-09-26 14:33:34 +0200

    --stats ready, working on arguments processing

---
+++ chars.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX_PWD_LEN 100
+
+void chars(){
+
+
+
+    while (fgets())
+    {
+        /* code */
+    }
+    
+}
+
+int main(){
+    
+}
--- pwcheck.c
+++ pwcheck.c
@@ -9,7 +9,6 @@
                                         -----------------------------------
                                                                          */
 
-// #TODO: дописать обработку уникальных символов
 
 // #define  _GNU_SOURCE
 #include <stdio.h>
@@ -17,10 +16,11 @@
 #include <stdbool.h>
 
 #define MAX_PWD_LEN 100
-#define MAX_UNIQUE_SYMBOLS 94 //ASCII TABLE 33-126
 #define empty ""
 #define help "--help"
 #define stats "--stats"
+#define one "1"
+#define two "2"
 
 enum ERRORS {
     PWD_LEN_ERROR = 1,
@@ -34,21 +34,6 @@ void showHelp(const char *argv){
     printf("--stats (optional) determines whether summary statistics of the analyzed passwords should be displayed at the end of the program\n--------\n");
 }
 
-bool compareStr(char *str, char *arg){
-    while (*str != '\0' || *arg != '\0')
-    {
-        if ((*str != *arg) || ((*str == '\0') && (*arg != '\0')) || ((*str != '\0') && (*arg == '\0')))
-        {
-            return false;
-        } else if (*str == *arg)
-        {
-            str++;
-            arg++;
-        }
-    }
-    return true;
-}
-
 int lengthStr(char str[MAX_PWD_LEN]){
     int i = 0;
     for (i = 0; str[i] != '\0' && str[i] != '\n'; ++i);
@@ -60,6 +45,21 @@ int lengthStr(char str[MAX_PWD_LEN]){
         }
     }
     return i;
+}
+
+bool compareStr(char *src, char *dst){
+    while (*src != '\0' || *dst != '\0')
+    {
+        if ((*src != *dst) || ((*src == '\0') && (*dst != '\0')) || ((*src != '\0') && (*dst == '\0')))
+        {
+            return false;
+        } else if (*src == *dst)
+        {
+            src++;
+            dst++;
+        }
+    }
+    return true;
 }
 
 bool helpEn(int argc, char **argv){
@@ -77,36 +77,56 @@ bool statsEn(int argc, char **argv){
     return false;
 }
 
-
-bool checkInt(char *arg){
-    return (*arg >= '0' && *arg <= '9') ? true : false;
+bool areIntegers(char str[MAX_PWD_LEN]){
+    for (int i = 0; i < lengthStr(str); i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool checkArgs(int argc, char **argv){
     for (int i = 1; i < argc-1; i++)
     {
-        if (argc == 4 && checkInt(argv[i]) && checkInt(argv[i+1]) && statsEn(argc, argv)) 
+        if (argc == 4 && areIntegers(argv[i]) && areIntegers(argv[i+1]) && statsEn(argc, argv)) 
             return true;
-        else if (argc == 3 && checkInt(argv[i]) && checkInt(argv[i+1])) 
+        else if (argc == 3 && areIntegers(argv[i]) && areIntegers(argv[i+1])) 
             return true;
     }
     return false;
 }
 
-bool areLetters(char str[MAX_PWD_LEN]){
+bool areLetters(char str[MAX_PWD_LEN], int param){
     int flagB, flagS;
     for (int i = 0; i < lengthStr(str); ++i)
     {
-        if (str[i] >= 65 && str[i] <= 90 )
+        if (param == 1)
         {
-            flagB = 1;
-        }
-        else if (str[i] >= 97 && str[i] <= 122)
+            if (str[i] >= 97 && str[i] <= 122)
+            {
+                return true;
+            }
+        } else if (param == 2)
         {
-            flagS = 1;
+            if (str[i] >= 65 && str[i] <= 90 )
+            {
+                return true;
+            }
+        } else if (param == 3)
+        {
+            if (str[i] >= 97 && str[i] <= 122)
+            {
+                flagS = 1;
+            }
+            if (str[i] >= 65 && str[i] <= 90 )
+            {
+                flagB = 1;
+            }
         }
     }
-    
     return (flagB == 1 && flagS == 1) ? true : false;
 }
 
@@ -115,49 +135,34 @@ int minLength(char str[MAX_PWD_LEN], int seen){
     return (currentMin <= seen) ? currentMin : seen;
 }
 
-int charsCount(char str[MAX_PWD_LEN]){
-    int seen[MAX_UNIQUE_SYMBOLS] = {0};
+int seenChars(){
+    rewind(stdin);
+    char password[MAX_PWD_LEN];
+    int seen[126] = {0};
     int n = 0;
-
-    for (int i = 0; i < lengthStr(str); ++i)
-    {   
-        if (str[i] != '\0' && str[i] != '\n' && str[i] != 1)
-        {
-            printf("str[i] is %d ", str[i]);
-            seen[(int)str[i]] = 1;
-            printf("seen[(int)str[i]] is %d\n", seen[(int)str[i]]);
+    while(fgets(password, MAX_PWD_LEN, stdin))
+    {
+        for (int i = 0; i < lengthStr(password); ++i)
+        {   
+            if (password[i] != '\0' && password[i] != '\n' && password[i] != 1)
+            {
+                seen[(int)password[i]] = 1;
+            }
         }
     }
-
-    
-    for (int i = 0; i < MAX_UNIQUE_SYMBOLS; ++i)
+    for (int i = 33; i < 126; ++i)
     {   
-        //char letter = seen[i];
-        // printf("seen[i] is '%d' ", seen[i]);
-        // printf("n is '%d'\n", seen[i]);
         n += seen[i];
     }
-
-    printf("n after password %s is %d\n", str, n);    
-    return n;
+    return n;    
 }
-
-// void showStats(int chars, int min, int avg){
-
-// }
-
-// void inputProcessing(){
-
-// }
 
 int main(int argc, char **argv){
 
     char password[MAX_PWD_LEN];
     int min = lengthStr(fgets(password, MAX_PWD_LEN, stdin));
     rewind(stdin);
-    float count = 0;
-    float avg;
-    float sum = 0;
+    float count = 0, avg, sum = 0;
     int chars;
 
     if (helpEn(argc, argv))
@@ -165,26 +170,59 @@ int main(int argc, char **argv){
         showHelp(argv[0]);
         return 0;
     } else if (checkArgs(argc, argv))
-    {   
+    {          
         while(fgets(password, MAX_PWD_LEN, stdin))
         {
             ++count;
             min = minLength(password, min);
             sum += lengthStr(password);
             avg = sum/count;
-            chars = charsCount(password);
-            printf("%s", password);
+
+            switch (*argv[1])
+            {
+                case '1':
+                    if (areLetters(password, 3))
+                    {
+                        printf("%s", password);
+                    } 
+                    break;
+                case '2':
+                    switch (*argv[2])
+                    {
+                    case '1':
+                        if (areLetters(password, 1))
+                        {
+                            printf("%s", password);
+                        }
+                        break;
+                    case '2':
+                        if (areLetters(password, 3))
+                        {
+                            /* code */
+                        }
+                        
+                    
+                    default:
+                        break;
+                    }
+                    break;
+                // case '3':
+                //     lv;
+                //     break;
+                // case '4':
+                //     lvl = 4;
+                //     break;
+                default: 
+                    printf("Argument %s has to be an integer int interval [1,4]\n", argv[1]);
+                    return 1;
+            }     
         }
-        printf("\n");
         
+        printf("\n");
         if (statsEn(argc, argv))
         {
-            
-            printf("Count: %.1f\n", count);
-            printf("Sum: %.1f\n", sum);
-            printf("Unique chars: %d\n", chars);
-            printf("Minimum length: %d\n", min);
-            printf("Average length: %.1f\n", avg);
+            chars = seenChars();
+            printf("Statistics:\nUnique chars: %d\nMinimum length: %d\nAverage length: %.1f\n", chars, min, avg);
         }
  
         return 0;
